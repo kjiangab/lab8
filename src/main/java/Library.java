ebe275838264a2d060111249883daa36190c32bb
jiang kuo <kjiangab@connect.ust.hk>
2019-04-11 09:25:38 +0800

    my second change

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,7 +3,7 @@
  */
 public class Library {
     public static void main(String arg[]) {
-    	System.out.println("hello world");
+    	System.out.println("hello ");
     }
     public boolean someLibraryMethod() {
     	return true;
